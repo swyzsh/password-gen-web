0733941308cd9ef87afbbdba4d3b4832f0348a81
Swyzsh <swyzsh@protonmail.com>
2024-09-29 19:01:43 +0530

    refactor generator settings to make it one page, switch grids animation with flickering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,35 @@
 import Generator from "@/components/Generator";
-import Navigation from "@/components/Navigation";
-import { Boxes } from "@/components/background-boxes";
+import FlickeringGrid from "@/components/FlickeringGrid";
 
 export default function Home() {
   return (
     <div className="m-0 p-6 h-screen overflow-x-hidden overflow-y-auto flex flex-col justify-start">
-      <div className="absolute inset-0 bg-[radial-gradient(circle,_rgba(28,28,28,0.9)_0%,_rgba(9,9,11,0.9)_100%)] pointer-events-none z-10" />
+      <div className="absolute inset-0 bg-[radial-gradient(circle,_rgba(30,41,59,0.5)_0%,_rgba(10,10,10,1.0)_100%)] pointer-events-none z-10" />
 
       <div
         className="z-20 relative mt-auto mb-4 ml-auto mr-auto h-1/3 w-8/12 max-2xl:w-9/12 max-xl:w-10/12 max-lg:w-11/12 max-md:w-full overflow-hidden
-                      border border-stone-700/30 rounded-lg flex flex-col justify-center items-center"
+                      border border-gray-900 rounded-lg flex flex-col justify-center items-center backdrop-blur-md"
       >
-        <Boxes />
-        <h1 className="z-30 p-6 text-[64px] max-md:text-[48px] text-center font-iosevkaMedium">Pass Gen</h1>
+        <FlickeringGrid
+          className="z-0 absolute inset-0 size-full"
+          squareSize={2}
+          gridGap={6}
+          color="#6b7280"
+          maxOpacity={0.5}
+          flickerChance={0.1}
+          width={1920}
+          height={1080}
+        />
+        <h1 className="z-30 pointer-events-none p-6 text-[64px] max-md:text-[48px] text-center font-iosevkaMedium">
+          Pass Gen
+        </h1>
       </div>
 
       <div
         className="mt-4 mb-auto ml-auto mr-auto w-8/12 max-2xl:w-9/12 max-xl:w-10/12 max-lg:w-11/12 max-md:w-full 
-                   p-6 border border-zinc-300/20 rounded-lg backdrop-blur-md z-50"
+                   p-8 border border-gray-900 rounded-lg backdrop-blur-md z-50"
       >
         <Generator />
-        <Navigation />
       </div>
     </div>
   );
--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -2,6 +2,7 @@
 import { useRef } from "react";
 import { useResponse } from "@/lib/useResponse";
 import { useCopyText } from "@/lib/useCopyText";
+import GeneratorSettings from "./GeneratorSettings";
 
 export default function Generator() {
   const { response, handleGenerate } = useResponse();
@@ -10,26 +11,34 @@ export default function Generator() {
 
   return (
     <div className="flex flex-col items-stretch justify-center">
-      <div className="flex flex-col justify-center items-center gap-3">
-        <div className="w-full flex flex-row items-center justify-center gap-4 rounded-lg border border-zinc-300/20 bg-zinc-500/20 px-4 py-2 text-center text-2xl text-zinc-100">
-          <output ref={outputRef} className="text-center break-all">
+      <div className="flex flex-row justify-center items-stretch gap-4">
+        <div
+          className="w-full flex flex-row items-stretch justify-between gap-4 rounded-lg border border-gray-900 bg-gray-950 
+                        px-4 py-2 text-gray-300"
+        >
+          <output ref={outputRef} className="text-2xl max-lg:text-xl max-md:text-lg text-start break-all">
             {response}
           </output>
           <button
             onClick={handleCopy}
-            className="rounded-lg border border-zinc-300/20 bg-zinc-500/20 px-2 py-1 text-xs"
+            className="px-2 py-1 text-sm max-lg:text-xs rounded-lg border border-gray-900 bg-gray-900 hover:border-gray-800"
           >
             Copy
           </button>
         </div>
-
         <button
           onClick={handleGenerate}
-          className="self-center rounded-lg border border-zinc-300/20 bg-zinc-500/20 px-2 py-1 text-center text-base text-zinc-100"
+          className="px-4 text-base max-lg:text-sm max-md:text-xs text-gray-300 inline-flex animate-shimmer items-center justify-center rounded-lg border border-gray-900 hover:border-gray-800
+                     bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] transition-colors focus:outline-none focus:ring-0 "
         >
           Generate
         </button>
       </div>
+      <GeneratorSettings />
     </div>
   );
 }
+
+/*
+          className="rounded-lg border border-zinc-300/20 bg-zinc-500/20 px-2 py-1 text-center text-base text-zinc-100"
+*/
